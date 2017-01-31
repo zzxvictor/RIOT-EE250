e95547efc98c66c0305d54b3b30178f6750a0daa
pedrohenriquegomes <pedrohenriquegomes@gmail.com>
2017-01-31 12:30:03 -0800

    Updating the python script to run on Wyliodrin

--- examples/openmote_adc/main.py
+++ examples/openmote_adc/main.py
@@ -1,38 +1,40 @@
 import time
 import serial
-import matplotlib.pyplot as plt
 
-# Set the DELAY in microseconds
-DELAY = 1000000
+######
+# This should be equal to DELAY on the firmware
+delay_microseconds = 500000
+######
 
 ser = serial.Serial(
-    port='\dev\ttyUSB0',\
+    port='/dev/ttyUSB0',\
     baudrate=115200,\
     parity=serial.PARITY_NONE,\
     stopbits=serial.STOPBITS_ONE,\
     bytesize=serial.EIGHTBITS,\
     timeout=0)
 
-x = []
-y = []    
-
-raw_input("Reset the OpenMote, then press Enter to continue...")
+raw_input("Press the reset button on the OpenMote, then press Enter to continue...")
 i = 1
-delay_seconds = (float)DELAY/1000000.0
-while True:
-    print "Running!\n"
-	
-	# Read the ADC value
-    line = ser.readline()
-	
-	# Parse it
-	value = int(line.split('@')[1])
 
-	y.append(value)
-	x.append(delay_seconds*i)
-	
-    print str(value) + '\n'
+delay_seconds = float(delay_microseconds) / 1000000.0
+
+while True:
+
+    # Read the ADC value
+    line = ser.readline()
+    while len(line.split('@')) < 3:
+        line = ser.readline()
+    
+    # Parse it
+    value = int(line.split('@')[1])
+
+    # Add to our vectors
+    y.append(value)
+    x.append(delay_seconds*i)
+    
+    print "ADC value: "+ str(value) + "\n"
+
     time.sleep(delay_seconds)
-	
-	i += 1
-	
+    i += 1
+    
