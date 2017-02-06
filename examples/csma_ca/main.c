b78195401aacb71c7e463472fa30abc179948dd3
pedrohenriquegomes <pedrohenriquegomes@gmail.com>
2017-02-06 13:54:58 -0800

    Changing files to have function that modify MinBO, MaxBO and Channel from the application

--- examples/csma_ca/main.c
+++ examples/csma_ca/main.c
@@ -5,21 +5,38 @@
 #include "shell.h"
 #include "shell_commands.h"
 
-#ifdef MODULE_NETIF
+#include "cc2538_rf.h"
+#include "cc2538_rf_netdev.h"
+
 #include "net/gnrc/pktdump.h"
 #include "net/gnrc.h"
-#endif
+
+#include "csma_ca_settings.h"
+
+/* Change the value of RFCORE_XREG_CCACTRL0 and RFCORE_XREG_CCACTRL1 register on 'csma_ca_settings.h' file to set the CCA theshold */
+
+/* Call the function below to change the Minimum Backoff Exponent */
+// set_csma_mac_min_be(val);
+
+/* Call the function below to change the Maximum Backoff Exponent */
+// set_csma_mac_max_be(val);
+
+/* Call the function below to change the channel of CC2538 */
+//cc2538_set_chan(ch);
 
 int main(void)
 {
-
-#ifdef MODULE_NETIF
     gnrc_netreg_entry_t dump;
-
     dump.pid = gnrc_pktdump_pid;
     dump.demux_ctx = GNRC_NETREG_DEMUX_CTX_ALL;
     gnrc_netreg_register(GNRC_NETTYPE_UNDEF, &dump);
-#endif
+
+    /* ***** */
+    /* Set here the channel, Min Backoff Exponent and Max Backoff Exponent
+    /* ***** */
+	//cc2538_set_chan(ch);
+	//set_csma_mac_min_be(val);
+	//set_csma_mac_max_be(val);
 
     (void) puts("Welcome to CSMA/CA test!");
 
