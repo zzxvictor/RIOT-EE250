c001a6eb073daf58d33057f1e4eebdcfc0a83ff6
Jason A. Tran <jasonatran@gmail.com>
2018-01-20 14:09:35 -0800

    adapt to RIOT updates up to jan 12, 2018

--- examples/udp_rx_tx/udp.c
+++ examples/udp_rx_tx/udp.c
@@ -12,7 +12,7 @@
  * @{
  *
  * @file
- * @brief       Simple program example to test channel statistics using UDP packets
+ * @brief       Simple program to test channel statistics using UDP packets
  *
  * @author      Jason A. Tran <jasontra@usc.edu>
  *              Pedro Henrique Gomes <pdasilva@usc.edu>
@@ -25,24 +25,28 @@
 
 #include "net/gnrc.h"
 #include "net/gnrc/ipv6.h"
+#include "net/gnrc/netif.h"
+#include "net/gnrc/netif/hdr.h"
 #include "net/gnrc/udp.h"
 #include "net/gnrc/pktdump.h"
 #include "timex.h"
+#include "utlist.h"
 #include "xtimer.h"
 
 #include "thread.h"
 #include "msg.h"
 
-#define QUEUE_SIZE 8
+#define MSG_QUEUE_SIZE 8
 #define CC2538_RSSI_OFFSET 73
 
 void print_rss(msg_t *msg);
 void print_prr(uint32_t pkt_rcv, uint32_t num_pkts);
 
-static gnrc_netreg_entry_t receiver = { NULL, GNRC_NETREG_DEMUX_CTX_ALL, {KERNEL_PID_UNDEF}};
+static gnrc_netreg_entry_t receiver = GNRC_NETREG_ENTRY_INIT_PID(GNRC_NETREG_DEMUX_CTX_ALL,
+                                                               KERNEL_PID_UNDEF);
 
 /* this is used to unregister the thread from receiving UDP packets sent to the 
-   port in the "receiver" struct */
+   port in the "receiver" struct. thread registry is done using a linked list. */
 static void _unregister_thread(void)
 {
     gnrc_netreg_unregister(GNRC_NETTYPE_UDP, &receiver);
@@ -63,6 +67,7 @@ int udp_tx(int argc, char **argv)
         return 1;
     }
 
+    int iface;
     unsigned int num_pkts = atoi(argv[1]);
     uint32_t interval = (uint32_t)atoi(argv[2]);
     ipv6_addr_t addr;
@@ -70,6 +75,12 @@ int udp_tx(int argc, char **argv)
 
     char *addr_str = argv[3];
     char *port_str = argv[4];
+
+    /* get interface, if available */
+    iface = ipv6_addr_split_iface(addr_str);
+    if ((iface < 0) && (gnrc_netif_numof() == 1)) {
+        iface = gnrc_netif_iter(NULL)->pid;
+    }
 
     /* parse destination address */
     if (ipv6_addr_from_str(&addr, addr_str) == NULL) {
@@ -87,12 +98,11 @@ int udp_tx(int argc, char **argv)
 
     uint8_t pkt_num = 1;
     for (unsigned int i = 0; i < num_pkts || num_pkts == 0; i++) {
-
         gnrc_pktsnip_t *payload, *udp, *ip;
         unsigned payload_size;
 
         /* allocate payload */
-        payload = gnrc_pktbuf_add(NULL, data_to_snd, 15, GNRC_NETTYPE_UNDEF);
+        payload = gnrc_pktbuf_add(NULL, data_to_snd, strlen(data_to_snd), GNRC_NETTYPE_UNDEF);
         if (payload == NULL) {
             puts("Error: unable to copy data to packet buffer");
             return 1;
@@ -117,6 +127,14 @@ int udp_tx(int argc, char **argv)
             return 1;
         }
 
+        /* add netif header, if interface was given */
+        if (iface > 0) {
+            gnrc_pktsnip_t *netif = gnrc_netif_hdr_build(NULL, 0, NULL, 0);
+
+            ((gnrc_netif_hdr_t *)netif->data)->if_pid = (kernel_pid_t)iface;
+            LL_PREPEND(ip, netif);
+        }
+
         /* send packet */
         if (!gnrc_netapi_dispatch_send(GNRC_NETTYPE_UDP, GNRC_NETREG_DEMUX_CTX_ALL, ip)) {
             puts("Error: unable to locate UDP thread");
@@ -124,7 +142,7 @@ int udp_tx(int argc, char **argv)
             return 1;
         }
 
-        printf("Success: send %u byte to [%s]:%u\n", payload_size, addr_str, port);
+        printf("Success: sent %u bytes to [%s]:%u\n", payload_size, addr_str, port);
 
         pkt_num++;
 
@@ -154,13 +172,12 @@ int udp_rx(int argc, char **argv)
     uint32_t port = atoi(argv[3]);
 
     msg_t msg; 
-    msg_t msg_queue[QUEUE_SIZE];
+    msg_t msg_queue[MSG_QUEUE_SIZE];
 
     /* setup the message queue */
-    msg_init_queue(msg_queue, QUEUE_SIZE);
+    msg_init_queue(msg_queue, MSG_QUEUE_SIZE);
 
     /* register this thread to the chosen UDP port */
-    receiver.next = NULL;
     receiver.demux_ctx = port; 
     receiver.target.pid = thread_getpid();
     gnrc_netreg_register(GNRC_NETTYPE_UDP, &receiver);
@@ -169,16 +186,18 @@ int udp_rx(int argc, char **argv)
 
     /* this function blocks. use msg_try_receive(&msg) for non-blocking. */
     msg_receive(&msg);
-    
+
     /* Print RSSI */
     print_rss(&msg);
 
     /* We initialize pkt_rcv with 1 */
     uint32_t pkt_rcv = 1;
+
+    printf("interval is %d\n", interval + interval/10);
     for (int i = 0; i < num_pkts - 1; i++) {
 
         /* We wait for an interval plus 10%. If no packet arrived, we have missed it */
-        if (xtimer_msg_receive_timeout(&msg, (uint32_t)((float)interval * 1.1)) < 0) {
+        if (xtimer_msg_receive_timeout(&msg, interval + interval/10) < 0) {
             continue;
         }
 
@@ -208,14 +227,14 @@ void print_rss(msg_t *msg)
         gnrc_pktsnip_t *snip = gnrc_pktsnip_search_type(pkt, GNRC_NETTYPE_NETIF);
         gnrc_netif_hdr_t *hdr = snip->data;
 
-        /* *** TO-DO *** */
+        /**** TO-DO ****/
 
-        /* gnrc_netif_hdr_t has information about the signal quality */
-        /* The RSS value is equal to the RSSI from 'hdr' minus CC2538_RSSI_OFFSET */
-        /* You must cast the RSSI from 'hdr' to signed integer because it is a 2-complement number */
-        /* You have to print out the RSS of each packet received */
-        
-
+        /** 
+         * gnrc_netif_hdr_t has information about the signal quality. The RSS 
+         * value is equal to the RSSI from 'hdr' minus CC2538_RSSI_OFFSET You 
+         * have to print out the RSS of each packet received. Feel free make any
+         * changes to help you finish the lab faster 
+        */
 
         /* Tell GNRC you are done with this packet so it can release the memory */
         gnrc_pktbuf_release(pkt);
@@ -224,11 +243,13 @@ void print_rss(msg_t *msg)
 
 void print_prr(uint32_t pkt_rcv, uint32_t num_pkts)
 {
-    /* *** TO-DO *** */
+    /**** TO-DO ****/
 
-    /* You know the number of packets you were supposed to receive */
-    /* and the number of packets you actually received. */
-    /* So, calculate what is the the Packet Reception Ratio and print out */
+    /** 
+     *You know the number of packets you were supposed to receive and the number
+     * of packets you actually received. Calculate the Packet Reception Ratio 
+     * and print it out 
+     */
     
 
 }
